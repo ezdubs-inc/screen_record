f80caaf04cc2d64e9024dbe2bc57b2a178e84274
Rohit Bernard <rohit@ezdubs.ai>
2025-04-04 14:41:26 -0700

    why ffmpeg, why

--- lib/src/create_video.dart
+++ lib/src/create_video.dart
@@ -2,8 +2,8 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:typed_data';
 
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit_config.dart';
+import 'package:ffmpeg_kit_flutter_audio/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_audio/ffmpeg_kit_config.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
